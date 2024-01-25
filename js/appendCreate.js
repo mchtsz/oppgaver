119a4de1e011c06646bfab5118f7f766a2e16698
Mchtsz <mhatland206@gmail.com>
2024-01-25 21:42:50 +0100

    javascript stuff

---
+++ js/appendCreate.js
@@ -0,0 +1,17 @@
+const foodArr = [
+    {
+        name: "burger",
+        price: 10,
+        img: "blablabla"
+    },
+    {
+        name: "pizza",
+        price: 20,
+        img: "blablabla"
+    }
+]
+
+const foodDiv = document.createElement("div");
+const foodH2 = document.createElement("h2");
+
+foodH2.textContent = "Food List";
